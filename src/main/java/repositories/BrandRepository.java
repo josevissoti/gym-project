8be427397332221aeeb07d866a9b48b1fddcd0ba
enzo-barbosa <pasquatty@gmail.com>
2025-05-22 16:17:36 -0300

    Add anotations on Repositories

--- src/main/java/repositories/BrandRepository.java
+++ src/main/java/repositories/BrandRepository.java
@@ -1,5 +1,10 @@
 package repositories;
 
-public interface BrandRepository {
+import domains.enums.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
 
 }
--- src/main/java/repositories/ProductRepository.java
+++ src/main/java/repositories/ProductRepository.java
@@ -1,4 +1,10 @@
 package repositories;
 
-public interface ProductRepository {
+import domains.enums.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
 }
