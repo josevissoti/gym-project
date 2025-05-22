a8bf73dbdae0b7d8e891d8d46ef94340cfce0ac2
enzo-barbosa <pasquatty@gmail.com>
2025-05-22 17:07:59 -0300

    Connect to database

--- src/main/java/com/project/academy/AcademyApplication.java
+++ src/main/java/com/project/academy/AcademyApplication.java
@@ -2,7 +2,9 @@ package com.project.academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"domains", "domains.enums"})
 @SpringBootApplication
 public class AcademyApplication {
 
