2a42feb9c37671757b640a222fb863435a4a8a82
josevissoti <josevissoti@gmail.com>
2025-05-25 21:58:57 -0300

    Create ServiceOrder Resource

--- src/main/java/com/project/resources/GymOrderResource.java
+++ src/main/java/com/project/resources/GymOrderResource.java
@@ -1,13 +1,15 @@
 package com.project.resources;
 
+import com.project.domains.GymOrder;
 import com.project.domains.dtos.GymOrderDTO;
 import com.project.services.GymOrderService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,6 +22,20 @@ public class GymOrderResource {
     @GetMapping
     public ResponseEntity<List<GymOrderDTO>> findAll() {
         return ResponseEntity.ok().body(gymOrderService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<GymOrderDTO> create(@Valid @RequestBody GymOrderDTO dto) {
+        GymOrder gymOrder = gymOrderService.create(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(gymOrder.getIdServiceOrder()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<GymOrderDTO> update(@PathVariable Long id, @Valid @RequestBody GymOrderDTO objDto) {
+        GymOrder Obj = gymOrderService.update(id, objDto);
+        return ResponseEntity.ok().body(new GymOrderDTO(Obj));
     }
 
 }
--- src/main/java/com/project/resources/SportOrderResource.java
+++ src/main/java/com/project/resources/SportOrderResource.java
@@ -1,13 +1,15 @@
 package com.project.resources;
 
+import com.project.domains.SportOrder;
 import com.project.domains.dtos.SportOrderDTO;
 import com.project.services.SportOrderService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,6 +22,20 @@ public class SportOrderResource {
     @GetMapping
     public ResponseEntity<List<SportOrderDTO>> findAll() {
         return ResponseEntity.ok().body(sportOrderService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<SportOrderDTO> create(@Valid @RequestBody SportOrderDTO dto) {
+        SportOrder sportOrder = sportOrderService.create(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(sportOrder.getIdServiceOrder()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<SportOrderDTO> update(@PathVariable Long id, @Valid @RequestBody SportOrderDTO objDto) {
+        SportOrderDTO Obj = sportOrderService.update(id, objDto);
+        return ResponseEntity.ok().body(new SportOrderDTO(Obj));
     }
 
 }
